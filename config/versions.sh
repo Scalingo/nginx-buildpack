fd0cc420e27d8f6fafc04bf90891b83382395835
Cédric Thomas <cedric.thomas@scalingo.com>
2023-01-04 13:56:03 +0100

    chore(versions): Bump versions

--- config/versions.sh
+++ config/versions.sh
@@ -1,4 +1,4 @@
 default_zlib_version="1.2.13"
-default_nginx_version="1.21.6"
-default_modsecurity_version="3.0.6"
-default_modsecurity_coreruleset_version="3.3.2"
+default_nginx_version="1.22.1"
+default_modsecurity_version="3.0.8"
+default_modsecurity_coreruleset_version="3.3.4"
