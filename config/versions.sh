e0f845feec4df56cc03306e836e79fd02c1c3251
François KUBLER <francois+github@kubler.org>
2025-11-12 11:05:06 +0100

    chore(nginx): release of nginx 1.29.3

--- config/versions.sh
+++ config/versions.sh
@@ -1,5 +1,5 @@
 default_zlib_version="1.3.1"
-default_nginx_version="1.29.0"
+default_nginx_version="1.29.3"
 default_modsecurity_version="3.0.14"
 default_modsecurity_nginx_version="1.0.4"
 default_modsecurity_coreruleset_version="4.19.0"
