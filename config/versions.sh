e7054fa387c5c5c23489abb00d2f15fa6b0f1ee0
François KUBLER <francois+github@kubler.org>
2026-05-18 10:28:54 +0200

    chore(nginx): release of nginx 1.30.1

--- config/versions.sh
+++ config/versions.sh
@@ -1,5 +1,6 @@
 default_zlib_version="1.3.2"
-default_nginx_version="1.29.8"
+# Keep a version from nginx' Stable branch:
+default_nginx_version="1.30.1"
 default_modsecurity_version="3.0.15"
 default_modsecurity_nginx_version="1.0.4"
 default_modsecurity_coreruleset_version="4.25.0"
